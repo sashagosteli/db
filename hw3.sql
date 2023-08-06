e1b163f7ac6a1133bf64d1403562c5c8bc2ca5a0
Alexandra <77861956+sashagosteli@users.noreply.github.com>
2023-08-06 16:05:23 +0200

    hw3.sql done

--- hw3.sql
+++ hw3.sql
@@ -11,14 +11,49 @@ INSERT INTO `sem3`.`stuff` (`id`, `firstname`, `lastname`, `post`, `seniority`, 
 INSERT INTO `sem3`.`stuff` (`id`, `firstname`, `lastname`, `post`, `seniority`, `salary`, `age`) VALUES ('10', 'Максим', 'Максимов', 'Рабочий', '2', '11000', '22');
 INSERT INTO `sem3`.`stuff` (`id`, `firstname`, `lastname`, `post`, `seniority`, `salary`, `age`) VALUES ('11', 'Юрий', 'Галкин', 'Рабочий', '3', '12000', '24');
 INSERT INTO `sem3`.`stuff` (`id`, `firstname`, `lastname`, `post`, `seniority`, `salary`, `age`) VALUES ('12', 'Людмила', 'Маркина', 'Уборщик', '10', '10000', '49');
+
+
 #1
+#Отсортируйте данные по полю заработная плата (salary) в порядке: убывания;
+#возрастания
 SELECT * FROM stuff
 ORDER BY salary;
 
 SELECT * FROM stuff
 ORDER BY salary DESC;
 
-#2
+#2 Выведите 5 максимальных заработных плат (saraly)
+
+SELECT salary FROM stuff
+ORDER BY salary DESC
+LIMIT 5;
+  
+#3. Посчитайте суммарную зарплату (salary) по каж дой специальности (роst)
+
+SELECT post, SUM(salary) SalaryCount
+FROM stuff
+GROUP BY post;
+
+#4. Найдите кол-во сотрудников с специальностью (post) « Рабочий» в возрасте от 24 до 49 лет включительно.
+  
+SELECT post, COUNT(*) PostWorker
+FROM stuff
+WHERE post = "Рабочий"
+AND age >= 24
+AND age <= 49
+GROUP BY post;
+
+#5. Найдите количество специальностей
+
+SELECT COUNT(DISTINCT post) AS CountPost
+FROM stuff;  
+  
+#6. Выведите специальности, у которых средний возраст сотрудников меньше 30 лет 
+
+SELECT post FROM stuff
+GROUP BY post
+HAVING AVG(age) <= 30;
+
 
 
 
