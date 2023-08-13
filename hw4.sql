06a696ad7ffa7d321da892e55d6eadf1344cb5c9
Alexandra <77861956+sashagosteli@users.noreply.github.com>
2023-08-13 16:26:31 +0200

    Update hw4.sql done

--- hw4.sql
+++ hw4.sql
@@ -7,3 +7,21 @@ UNION SELECT MARK, COLOR, COUNT(COLOR) AS 'AUTOquantity' FROM AUTO
 WHERE MARK = 'BMW'
 GROUP BY COLOR, MARK;
 
+-- Вывести на экран марку авто и количество AUTO не этой марки
+
+SELECT COUNT(MARK) AllMarkCount,
+		COUNT(CASE WHEN MARK != 'BMW' THEN mark END) CountWithoutBMW,
+        COUNT(CASE WHEN MARK != 'LADA' THEN mark END) CountWithoutLADA,
+		COUNT(CASE WHEN MARK != 'AUDI' THEN mark END) CountWithoutAUDI,
+        COUNT(CASE WHEN MARK != 'VOLVO' THEN mark END) CountWithoutVOLVO
+FROM AUTO
+;
+
+
+
+-- Напишите запрос, который вернет строки из таблицы test_a, id которых нет в таблице test_b, НЕ используя ключевого слова NOT.
+
+SELECT * FROM test_a
+NATURAL LEFT JOIN test_b
+WHERE test_b.id_number IS NULL;
+
