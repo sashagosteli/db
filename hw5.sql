aeb971cc21d5e55807211a232718122a51792d16
Alexandra <77861956+sashagosteli@users.noreply.github.com>
2023-08-27 20:50:20 +0200

    Update and rename HW5 to hw5.sql

--- hw5.sql
+++ hw5.sql
@@ -1,10 +1,30 @@
+-- 1. Создайте представление, в которое попадет информация о пользователях (имя, фамилия,
+-- город и пол), которые не старше 20 лет.
+use seminar_4;
+CREATE OR REPLACE VIEW users_VIEW AS 
+	SELECT 
+   	u.id as id,
+   	u.firstname as firstname,
+	u.lastname as lastname,
+    
+    	p.hometown as hometown,
+    	p.gender as gender,
+    	p.birthday as b_date
+   
+	from users u
+RIGHT join profiles p on u.id = p.user_id
+WHERE DATE(birthday) > '2003-08-27'
+
+ORDER BY p.birthday;
+
+SELECT * FROM users_VIEW;
+
+
 -- 2. Найдите кол-во, отправленных сообщений каждым пользователем и выведите
 -- ранжированный список пользователей, указав имя и фамилию пользователя, количество
 -- отправленных сообщений и место в рейтинге (первое место у пользователя с максимальным
 -- количеством сообщений) . (используйте DENSE_RANK)
 
-
-use semimar_4;
 SELECT
 	m.from_user_id ,
     COUNT(m.from_user_id) as count_msg,
