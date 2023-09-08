82556e43a6b5766930f55bc6e9b07b6bd4e730d7
Alexandra <77861956+sashagosteli@users.noreply.github.com>
2023-09-08 14:43:22 +0200

    Update hw6.sql

--- hw6.sql
+++ hw6.sql
@@ -25,3 +25,41 @@ SET @user = 3;
 CALL into_users_old();
 
 SELECT * FROM users_old;
+
+
+
+
+--TASK 2
+
+
+DROP FUNCTION IF EXISTS hello;
+SET @now = current_time();
+DELIMITER //
+ 
+
+ 
+CREATE FUNCTION `hello` (now time)
+RETURNS CHAR(45) READS SQL DATA
+BEGIN
+ DECLARE answer varchar(45);
+ DECLARE now time;
+--  SET @now = current_time();
+  IF (@now >= "06:00:00")AND (@now < "12:00:00") THEN 
+	SET answer = "Good Morning";
+ ELSEIF (@now >= "12:00:00")AND (@now < "18:00:00") THEN 
+	SET answer = "Good Afternoon";
+ ELSEIF (@now >= "18:00:00")AND (@now < "00:00:00") THEN 
+	SET answer = "Good Evening";
+ ELSEIF (@now >= "00:00:00")AND (@now < "06:00:00") THEN 
+	SET answer = "Good Night" ;
+ END IF;
+ 
+RETURN answer;
+ 
+END //
+ 
+DELIMITER ;
+
+SELECT hello(@now);
+
+
